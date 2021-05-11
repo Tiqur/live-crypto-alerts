ad28d5aed731eb987b1ddbf75f44ee7cdea5fbe4
Tiqur <trevorbrage02@gmail.com>
2021-05-11 01:55:30 -0700

    Removed unnecessary imports

--- indicators/ema.py
+++ indicators/ema.py
@@ -1,7 +1,4 @@
-import numpy as np 
 
-
-# https://stackoverflow.com/a/42915307
 def np_ema(current_price, prev_ema, window):
     k = 2 / ( window + 1 )
     return current_price * k + prev_ema * (1 - k)
