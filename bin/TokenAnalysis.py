c1d3e09e439281c90a1821e43092de9edf15f023
Tiqur <trevorbrage02@gmail.com>
2021-05-11 18:10:54 -0700

    Optimized download ranges

--- bin/TokenAnalysis.py
+++ bin/TokenAnalysis.py
@@ -16,27 +16,31 @@ class TokenAnalysis():
         self.history = []
 
     def download_history(self):
-        # Emas rely on previous emas.  Calculate extra for more precision
-        download_range = max(self.ma_intervals) * self.precision
         
         # Download data for each time_interval and moving average range
         for time_interval in self.time_intervals:
             print(f"Downloading {time_interval} data for {self.token} {self.time_intervals.index(time_interval)+1}/{len(self.time_intervals)}")
 
-            # Convert binance kline to seconds
-            interval_sec = interval_to_sec(time_interval) * download_range
-            current_time = time.time()
-            start_time = current_time - interval_sec
-            historical_data = self.client.get_historical_klines(self.token, time_interval, str(start_time), str(current_time))
-    
-            # Initialize interval history
-            ih = IntervalHistory(time_interval)
+            # Optimize downloads by only downloaded the necessary data per token
+            for ma_interval in self.ma_intervals:
 
-            # Organize data
-            for data in historical_data:
-                ih.ohlcv.append(Ohlvc(data))
-            
-            self.history.append(ih)
+                # Emas rely on previous emas.  Calculate extra for more precision
+                download_range = ma_interval * self.precision
+
+                # Convert binance kline to seconds
+                interval_sec = interval_to_sec(time_interval) * download_range
+                current_time = time.time()
+                start_time = current_time - interval_sec
+                historical_data = self.client.get_historical_klines(self.token, time_interval, str(start_time), str(current_time))
+        
+                # Initialize interval history
+                ih = IntervalHistory(time_interval)
+
+                # Organize data
+                for data in historical_data:
+                    ih.ohlcv.append(Ohlvc(data))
+                
+                self.history.append(ih)
 
     def calc_emas(self):
         for time_interval in self.history:
--- main.py
+++ main.py
@@ -22,11 +22,12 @@ precision = 4
 time_intervals = [Client.KLINE_INTERVAL_1MINUTE, Client.KLINE_INTERVAL_3MINUTE, Client.KLINE_INTERVAL_5MINUTE, Client.KLINE_INTERVAL_15MINUTE, Client.KLINE_INTERVAL_30MINUTE]
 
 
+# Start websocket connections ( get live token data )
 live_updates_thread = threading.Thread(target=live_updates, args=(time_intervals, watchlist, 'binance.com'))
 live_updates_thread.start()
 
 
-
+# Download historical token data
 for token in watchlist:
     token_analysis = TokenAnalysis(client, token, time_intervals, ma_intervals, precision)
     token_analysis.download_history()
