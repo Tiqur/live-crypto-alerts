4c21e7507015fabe9efc5ed73398d422480c925d
Tiqur <trevorbrage02@gmail.com>
2021-05-11 18:01:18 -0700

    Made websocket function parameters dynamic

--- bin/live_updates.py
+++ bin/live_updates.py
@@ -1,8 +1,9 @@
 from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
 
-def live_updates():
-    binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.com")
-    binance_websocket_api_manager.create_stream(['kline_1m'], ['dogeusdt'])
+def live_updates(intervals, tokens, exchange):
+    binance_websocket_api_manager = BinanceWebSocketApiManager(exchange=exchange)
+    parsed_intervals = list(map(lambda i: f"kline_{i}", intervals))
+    binance_websocket_api_manager.create_stream(parsed_intervals, tokens)
 
     # Actually get the live data
     while True:
--- main.py
+++ main.py
@@ -19,10 +19,10 @@ ma_intervals = [9, 13, 21, 55]
 # Amount of data relative to maximum interval to download. ( max_interval * precision )
 # So if max_interval equals 55 and precision equals 2, then it would download 110 records for each interval
 precision = 4
-time_intervals = [Client.KLINE_INTERVAL_1MINUTE]
+time_intervals = [Client.KLINE_INTERVAL_1MINUTE, Client.KLINE_INTERVAL_3MINUTE, Client.KLINE_INTERVAL_5MINUTE, Client.KLINE_INTERVAL_15MINUTE, Client.KLINE_INTERVAL_30MINUTE]
 
 
-live_updates_thread = threading.Thread(target=live_updates)
+live_updates_thread = threading.Thread(target=live_updates, args=(time_intervals, watchlist, 'binance.com'))
 live_updates_thread.start()
 
 
@@ -31,6 +31,7 @@ for token in watchlist:
     token_analysis = TokenAnalysis(client, token, time_intervals, ma_intervals, precision)
     token_analysis.download_history()
     token_analysis.calc_emas()
+
 
 
 
