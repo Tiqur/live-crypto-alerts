ed964560000ef3dee845444a7b411220003182ed
Tiqur <trevorbrage02@gmail.com>
2021-05-13 13:50:13 -0700

    Fix buy signal on startup

--- bin/TimeInterval.py
+++ bin/TimeInterval.py
@@ -2,7 +2,7 @@ class TimeInterval():
     def __init__(self, interval):
         self.candle_time_interval = interval
         self.moving_average_instances = []
-        self.flag = 0
+        self.flag = 1
 
     def __repr__(self):
         return(f"""
